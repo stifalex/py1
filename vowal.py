6de0c6c2173a93ff9375790a71395ffc1dd49b27
stifalex <51439361+stifalex@users.noreply.github.com>
2019-06-06 16:32:07 +0530

    Update vowal.py

--- vowal.py
+++ vowal.py
@@ -1,4 +1,4 @@
-ch = input("Enter a character: ")
+ch = input()
 if((ch>='a' and ch<='z')or(ch>='A' and ch<='Z')):
     if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
         print("Vowel")
